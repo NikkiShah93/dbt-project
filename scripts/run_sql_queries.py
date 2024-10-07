0edb19a6656740e75ed4c33a064c7a36b5df655b
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-10-07 14:21:25 -0400

    initial commit

--- scripts/run_sql_queries.py
+++ scripts/run_sql_queries.py
@@ -3,9 +3,20 @@ import duckdb
 import pandas as pd
 
 CWD = os.getcwd()
+
 sql_query = """
 SHOW TABLES;
 """
+with open('scripts/sql_statements/create_lookup.sql') as s:
+    q1 =''
+    for line in s.readlines():
+        q1+=line
+with open('scripts/sql_statements/create_main_table.sql') as s:
+    q2 =''
+    for line in s.readlines():
+        q2+=line
 
 with duckdb.connect('data/test_db.db') as con:
+    con.sql(q1)
+    con.sql(q2)
     print(con.sql(sql_query).df())
